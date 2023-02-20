39101de432dab84aa34d2fe297eaa2d307cc8c89
afolaode <afolaode@gmail.com>
2023-02-20 13:58:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Question 3
+
+function findnearestPwr(num) {
+  let pwr;
+  for (let i = 1; i < num; i ** 2) {
+    pwr = i;
+  }
+}
+
+console.log(`The number ${pwr} is the power of 2 nearest to ${num}.`);
+
+// Question 5
+
+`let classGroup = "Arts";``if (classGroup === "Science") {
+  console.log("Your science subjects are: Physics, Chemistry, Biology, Technical Drawing");
+} else if (classGroup === "Social Science") {
+  console.log("Your social science subjects are: Accounting, Commerce, Marketing, Geography");
+} else if (classGroup === "Arts") {
+  console.log("Your arts subjects are: Government, Economics, Literature, History");
+} else {
+  console.log("Invalid class group. Your general subjects are: English, Mathematics");
+}`;
